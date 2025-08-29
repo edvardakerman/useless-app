e9a42a22ab42fe231012536fc7da1cd55fdfa570
edvardakerman <ed@sagovagen.se>
2025-08-29 11:20:43 +0200

    reset config and package.json

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   crossOrigin: 'anonymous',
-  reactStrictMode: true,
-  distDir: 'build',
-  output: 'standalone',
+
 };
 
 export default nextConfig;
