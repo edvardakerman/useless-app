53f1113c3344112adf0764019ece368bb59437c0
edvardakerman <ed@sagovagen.se>
2025-08-29 11:13:21 +0200

    node engiens v18.x package.json

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   crossOrigin: 'anonymous',
-
+  reactStrictMode: true,
+  distDir: 'build',
+  output: 'standalone',
 };
 
 export default nextConfig;
